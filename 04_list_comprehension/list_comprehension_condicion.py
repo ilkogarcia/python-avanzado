513e4b1c9460d0d0c9ad48b2e1559016370685a6
Ilko García <ilko.garcia@gmail.com>
2022-07-26 09:30:05 +0200

    Uso condiciones en list comprehension

---
+++ 04_list_comprehension/list_comprehension_condicion.py
@@ -0,0 +1,18 @@
+'''
+lista = [ expresion(elemento) for elemento in objetoiterable if condición ]
+lista = [ expresion(condición) for elemento in objeto_iterable ]
+'''
+
+def calcular_cuadrado(numero):
+    return numero**2
+
+
+def es_par(numero):
+    return numero % 2 == 0
+
+lista_num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+lista_cuadrados = [calcular_cuadrado(num) for num in lista_num]
+print(lista_cuadrados)
+
+lista_cuadrados_pares = [calcular_cuadrado(num) for num in lista_num if es_par(num)]
+print(lista_cuadrados_pares)
