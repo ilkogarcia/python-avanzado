3187933ea8b712ad5725d2fc1ed82421107a5c1e
Ilko García <ilko.garcia@gmail.com>
2022-07-26 09:22:12 +0200

    Actualizada la documentación en el encabezado

--- 04_list_comprehension/list_comprehension.py
+++ 04_list_comprehension/list_comprehension.py
@@ -1,5 +1,12 @@
 '''
 lista = [ expresion(elemento) for elemento in objeto_iterable ]
+
+list comprehension nos permiten crear listas de elementos en una
+sola línea de código. Por ejemplo, podemos crear una lista con
+los cuadrados de los primeros 5 números de la siguiente forma
+
+cuadrados = [i**2 for i in range(5)]
+#[0, 1, 4, 9, 16]
 '''
 
 def calcular_cuadrado(numero):
@@ -26,3 +33,9 @@ print(lista_cuadrados_pares)
 lista_resultados = [calcular_cuadrado(num) if es_par(num) else 0 for num in lista_num]
 print(lista_resultados)
 
+# Ejemplo en el que vamos a determinar el número de "r" e una frase
+frase = "El perro de san roque no tiene rabo"
+erres = [i for i in frase if i == 'r']
+#['r', 'r', 'r', 'r']
+print(len(erres))
+#4
--- 04_list_comprehension/map.py
+++ 04_list_comprehension/map.py
@@ -1,3 +1,25 @@
+'''
+map() function returns a map object(which is an iterator)
+of the results after applying the given function to each
+item of a given iterable (list, tuple etc.)
+
+Syntax:
+map(fun, iter)
+
+Parameters:
+- fun: It is a function to which map passes each element
+of given iterable.
+- iter: It is a iterable which is to be mapped.
+
+NOTE: You can pass one or more iterable to the map() function.
+
+Returns:
+Returns a list of the results after applying the given function  
+to each item of a given iterable (list, tuple etc.) 
+ 
+NOTE: The returned value from map() (map object) then can be passed to functions like list() (to create a list), set() (to create a set) .
+'''
+
 def calcular_cuadrado(numero):
     return numero**2
 
