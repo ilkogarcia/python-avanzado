219cb9a5891b8313df8ae55f88c640efca338bdc
Ilko García <ilko.garcia@gmail.com>
2022-07-26 09:34:53 +0200

    Uso de list comprehension con if-else

--- 04_list_comprehension/list_comprehension_condicion.py
+++ 04_list_comprehension/list_comprehension_condicion.py
@@ -14,5 +14,10 @@ lista_num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 lista_cuadrados = [calcular_cuadrado(num) for num in lista_num]
 print(lista_cuadrados)
 
+# Crea una lista con el cuadrado de los números pares de una lista
 lista_cuadrados_pares = [calcular_cuadrado(num) for num in lista_num if es_par(num)]
 print(lista_cuadrados_pares)
+
+# Otra forma de crear una lista con el cuadrado de los números pares en una lista
+lista_resultados = [calcular_cuadrado(num) if es_par(num) else 0 for num in lista_num]
+print(lista_resultados)
